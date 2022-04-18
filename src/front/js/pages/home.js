ab73e05e3575c8d0720f11e8caabfb3e2babd433
DaniBorrero <borrerodaniel89@gmail.com>
2022-04-18 17:45:15 +0000

    dandole formato al carrusel, faltan detalles

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,28 +9,30 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<Carousel>
+		<Carousel className="container">
 			<Carousel.Item>
+				
 				<img
 				className="d-block w-100"
-				src="https://images8.alphacoders.com/115/thumb-1920-1156488.png"				alt="First slide"
+				src="https://p4.wallpaperbetter.com/wallpaper/842/836/906/web-development-minimalism-wallpaper-preview.jpg"				alt="First slide"
 				/>
 				<Carousel.Caption>
+				<p>Soy un aprendiz en el mundo del desarrollo web, apasionado por seguir aprendiendo y crecer como profesional. Actualmente estoy en busca de nuevos desafíos en el mundo del desarrollo de software, me considero una persona adaptable, que puede trabajar en equipo y dispuesto a seguir aprendiendo. Actualmente estoy aprendiendo sobre React-Native y typescrip gracias a un curso en Udemy.</p>
 				<h3>¿Quien soy?</h3>
-				<p>Una pequeña descripcion de mi personas
+				<p>Una pequeña descripcion de mi persona
 				</p>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img
 				className="d-block w-100"
-				src="https://images8.alphacoders.com/115/thumb-1920-1156488.png"
+				src="https://p4.wallpaperbetter.com/wallpaper/381/165/306/simple-background-black-background-minimalism-digital-art-wallpaper-preview.jpg"
 				alt="Second slide"
 				/>
 
 				<Carousel.Caption>
-				<h3>Second slide label</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<h3>Lenguajes que conozco</h3>
+				<p>Agregar iconos de los lenguajes que conozco</p>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
@@ -40,8 +42,8 @@ export const Home = () => {
 				/>
 
 				<Carousel.Caption>
-				<h3>Third slide label</h3>
-				<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+				<h3>habilidades blandas</h3>
+				<p>Mas alla de la programacion</p>
 				</Carousel.Caption>
 			</Carousel.Item>
 		</Carousel>
